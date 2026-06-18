c2b7006da3e46dddffa3dc7c488be90df75d249a
i-shaurya1912 <vshaurya1912@gmail.com>
2026-06-18 14:31:12 +0530

    enhance the code

--- script.js
+++ script.js
@@ -1,41 +1,57 @@
-const colorCodeContainer =document.getElementById("color-code")
+const colorCodeContainer = document.getElementById("color-code")
 const optionContainer = document.getElementById("options-container")
 
 let randomColor = null;
 
 let score = 0;
 
-
-
 function generateRandomNumberBetween(min, max) {
-    return min + Math.floor(Math.random()* (max - min + 1));
+    return min + Math.floor(Math.random() * (max - min + 1));
 }
 
-function generateRandomColorRGB(){
-const red = generateRandomNumberBetween(0,255);
-const green = generateRandomNumberBetween(0,255);
-const blue = generateRandomNumberBetween(0,255);
-return `rgb(${red}, ${green}, ${blue})`;
+function generateRandomColorRGB() {
+    const red = generateRandomNumberBetween(0, 255);
+    const green = generateRandomNumberBetween(0, 255);
+    const blue = generateRandomNumberBetween(0, 255);
+
+    return `rgb(${red}, ${green}, ${blue})`;
+}
+
+function incrementScore() {
+    score += 1;
+    document.getElementById('score').innerText = score;
 }
 
 function validateResult(el) {
-  
-   const selectedColor = el.target.styl.backgroundColor;
-   console.log(selectedColor ===randomColor);
+    const selectedColor = el.target.style.backgroundColor;
+
+    if (selectedColor === randomColor) {
+        incrementScore();
+        startGame();
+    }
 }
 
-function startGame(){
+function startGame() {
+    optionContainer.innerHTML = "";
+
     randomColor = generateRandomColorRGB();
     colorCodeContainer.innerText = randomColor;
 
     const ansIndex = generateRandomNumberBetween(0, 5);
-     
 
-    for ( let i = 0; i < 6; i++) {
+    for (let i = 0; i < 6; i++) {
         const div = document.createElement('div');
-       
-        div.style.backgroundColor = generateRandomColorRGB();
+
+        if (i === ansIndex) {
+            div.style.backgroundColor = randomColor;
+        } else {
+            div.style.backgroundColor = generateRandomColorRGB();
+        }
+
+        div.addEventListener("click", validateResult);
+
         optionContainer.append(div);
     }
 }
-window.addEventListener("load" , () => startGame())
+
+window.addEventListener("load", () => startGame())
